d8e53e21bb325c5e08cfc09819a7a26f68acb4c9
Piotrek Żurek <piotrek.zurek+gitlab@gmail.com>
2022-11-03 00:38:03 +0100

    copy over mdadm, try to boot from kernel option only

--- build_initramfs.sh
+++ build_initramfs.sh
@@ -40,6 +40,7 @@ cp $(ldd "/sbin/e2fsck" | egrep -o '/.* ') ${INITRAMFS_ROOT}/lib/
 cp -a /sbin/*btrfs ${INITRAMFS_ROOT}/sbin
 cp -a /bin/btrfs* ${INITRAMFS_ROOT}/bin
 cp -a /lib/x86_64-linux-gnu/liblzo2.so.2  ${INITRAMFS_ROOT}/lib/x86_64-linux-gnu/liblzo2.so.2
+cp -a /sbin/mdadm ${INITRAMFS_ROOT}/sbin
 
 cat << EOF > ${INITRAMFS_ROOT}/init
 #!/bin/busybox sh
@@ -88,38 +89,38 @@ for param in \$(cat /proc/cmdline); do
 	esac
 done
 
-mdadm --assemble --scan
+/sbin/mdadm --assemble --scan
 
-# try to mount the root filesystem.
+# try to mount the root filesystem from kernel options
 if [ "\${root}"x != "/dev/ram"x ]; then
 	mount -t \${rootfstype} -o \${ro},\${rootflags} \${root} /newroot || rescue_shell "mount \${root} failed."
 fi
 
 # try 2nd partition on usb
-if [ ! -x /newroot/\${init} ] && [ ! -h /newroot/\${init} ] && [ -b /dev/sda1 ] && [ -b /dev/sda2 ]; then
-	mount -t \${rootfstype} -o \${ro},\${rootflags} /dev/sda2 /newroot
-	if [ ! -x /newroot/\${init} ] && [ ! -h /newroot/\${init} ]; then
-		umount /dev/sda2
-	fi
-fi
+# if [ ! -x /newroot/\${init} ] && [ ! -h /newroot/\${init} ] && [ -b /dev/sda1 ] && [ -b /dev/sda2 ]; then
+# 	mount -t \${rootfstype} -o \${ro},\${rootflags} /dev/sda2 /newroot
+# 	if [ ! -x /newroot/\${init} ] && [ ! -h /newroot/\${init} ]; then
+# 		umount /dev/sda2
+# 	fi
+# fi
 
-# try 1st partition on hdd
-if [ ! -x /newroot/\${init} ] && [ ! -h /newroot/\${init} ] && [ -b /dev/sda1 ]; then
-	mount -t \${rootfstype} -o \${ro},\${rootflags} /dev/sda1 /newroot
-	if [ ! -x /newroot/\${init} ] && [ ! -h /newroot/\${init} ]; then	
-		umount /dev/sda1
-	fi
-fi
+# # try 1st partition on hdd
+# if [ ! -x /newroot/\${init} ] && [ ! -h /newroot/\${init} ] && [ -b /dev/sda1 ]; then
+# 	mount -t \${rootfstype} -o \${ro},\${rootflags} /dev/sda1 /newroot
+# 	if [ ! -x /newroot/\${init} ] && [ ! -h /newroot/\${init} ]; then	
+# 		umount /dev/sda1
+# 	fi
+# fi
 
 
-# try 3rd partition on hdd
-if [ ! -x /newroot/\${init} ] && [ ! -h /newroot/\${init} ] && [ -b /dev/sda1 ] && [ -b /dev/sda3 ]; then
-	mount -t \${rootfstype} -o \${ro},\${rootflags} /dev/sda3 /newroot || rescue_shell "mount \${root} failed."
-	if [ ! -x /newroot/\${init} ] && [ ! -h /newroot/\${init} ]; then
-		umount /dev/sda3
-		rescue_shell "nothing bootable"
-	fi
-fi
+# # try 3rd partition on hdd
+# if [ ! -x /newroot/\${init} ] && [ ! -h /newroot/\${init} ] && [ -b /dev/sda1 ] && [ -b /dev/sda3 ]; then
+# 	mount -t \${rootfstype} -o \${ro},\${rootflags} /dev/sda3 /newroot || rescue_shell "mount \${root} failed."
+# 	if [ ! -x /newroot/\${init} ] && [ ! -h /newroot/\${init} ]; then
+# 		umount /dev/sda3
+# 		rescue_shell "nothing bootable"
+# 	fi
+# fi
 
 # WD My Cloud: turn led solid blue
 #echo none > /sys/class/leds/system-blue/trigger
